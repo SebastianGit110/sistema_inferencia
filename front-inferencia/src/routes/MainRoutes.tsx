6d1554651d59b6550efa38dc12cb820dccf30324
Sebastian <sebastian.hernandorv@gmail.com>
2025-11-09 19:30:56 -0500

    cambioss

--- front-inferencia/src/routes/MainRoutes.tsx
+++ front-inferencia/src/routes/MainRoutes.tsx
@@ -1,7 +1,7 @@
 import { Navigate, RouteObject } from "react-router-dom";
 import App from "../App";
 import Login from "../auth/Login";
-import Search from "../inference/search";
+import Search from "../inference/Search";
 import { Admin } from "../inference/Admin";
 
 export const MainRoutes: RouteObject[] = [
@@ -9,7 +9,7 @@ export const MainRoutes: RouteObject[] = [
     path: "/",
     element: <App />,
     children: [
-      { path: "", element: <Navigate to={"login"} /> },
+      // { path: "", element: <Navigate to={"login"} /> },
       { path: "login", element: <Login /> },
       { path: "search", element: <Search /> },
       { path: "admin", element: <Admin /> },
