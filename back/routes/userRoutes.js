3f7ce689c505f2f555202125df8ced09c20de784
Felipe <fcfelipecruz05@gmail.com>
2025-11-18 21:36:42 -0500

    admin completo

--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getUsers, updateUserRole, deleteUser } from '../controllers/userController.js';
+import { getUsers, getUserById, updateUserRole,  updateUser, deleteUser,createUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.put('/:id/rol', updateUserRole);
 router.delete('/:id', deleteUser);
+router.put('/:id', updateUser);
+router.get('/:id', getUserById);
+router.post('/', createUser);
 
 export default router;
